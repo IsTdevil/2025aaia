4a77dd5b261d2ddb46e28564ee20cbb992bc7740
IsTdevil <euan1014@gmail.com>
2025-02-21 12:06:18 +0800

    week01-1

---
+++ week01-1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> ///使用standard 標準的input /output
+
+int main()
+{
+	int a,b;///整數a,b
+	scanf( "%d%d",&a,&b );///毒入資料 "%d%d"後面有逗號
+	///兩個%d%d & &
+	printf("%d" ,a + b );///印出資料,把答案印出來
+}
